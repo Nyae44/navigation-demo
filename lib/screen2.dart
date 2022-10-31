41a19a197144c7270fefc491828c9b94cd4e98af
daley <tingadaley@gmail.com>
2022-10-31 04:02:46 +0300

    widget lifecycle

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -2,11 +2,31 @@
 
 import 'package:flutter/material.dart';
 
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget {
   const Screen2({super.key});
 
   @override
+  State<Screen2> createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    debugPrint('initState Called');
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    debugPrint('deactivate called');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    debugPrint('buid called');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
